f3e2e9357437a8788753a9ea1911e3a9b57859c5
Steven Bateman <batemanvo@gmail.com>
2016-12-14 18:45:00 -0800

    Began work on server.js.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer();
+
+server.listen(PORT, () => {
+  console.log('Server running on port', PORT);
+});
