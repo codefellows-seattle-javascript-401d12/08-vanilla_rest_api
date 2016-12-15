02bd12661f184cdc7dbce39d8f3a7a964bd222ec
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:08:21 -0800

    Added route function to Router prototype.

--- lab-steven/lib/router.js
+++ lab-steven/lib/router.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const parseURL = require('./url-parser.js');
+const parseUrl = require('./url-parser.js');
 const parseJSON = require('./body-parser.js');
 
 const Router = module.exports = () => {
@@ -26,4 +26,32 @@ Router.prototype.put = (endpoint, callback) => {
 
 Router.prototype.delete = (endpoint, callback) => {
   this.routes.DELETE[endpoint] = callback;
+};
+
+Router.prototype.route = () => {
+  return (request, response) => {
+    Promise.all([
+      parseUrl(request),
+      parseJSON(request)
+    ])
+    .then(() => {
+      if (typeof this.routes[request.method][request.url.pathname] === 'function') {
+        this.routes[request.method][request.url.pathname](request, response);
+        return;
+      }
+
+      console.error('No such route exists.');
+
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write('No such route exists.');
+      response.end();
+    })
+    .catch(err => {
+      console.error(err);
+
+      response.writeHead(400, {'Content-Type': 'text/plain'});
+      response.write('Bad request.');
+      response.end();
+    });
+  };
 };
