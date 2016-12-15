bb1bfa5cef7bc40aebad407f0c968c2c055f2c6a
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:16:43 -0800

    Imported all dependencies to server.js.

--- lab-steven/server.js
+++ lab-steven/server.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const http = require('http');
+const Student = require('./model/student-constructor.js');
+const Router = require('./lib/router.js');
+const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 8080;
+const router = new Router();
 
 const server = http.createServer();
 
