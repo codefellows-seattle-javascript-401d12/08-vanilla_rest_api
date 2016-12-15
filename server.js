49b00dfe528b24dd3bf4cf7598d0b9dbedf8228c
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-15 12:44:17 -0800

    add new module consts

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const http = require('http');
+const BEV = require('./model/bevs.js');
+const Router = require('./lib/router.js');
 const PORT = process.env.PORT || 3000;
+const router = new Router();
 
 const server = http.createServer();
 
