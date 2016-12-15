7625a57a11fbe2eac487691083b76f3401dcece3
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:15:24 -0800

    Created storage mimic with createItem and getItem functions.

---
+++ lab-steven/lib/storage.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const storage = {};
+
+module.exports = exports = {};
+
+exports.createItem = (schemaName, item) => {
+  if (!schemaName) return Promise.reject(new Error('No schema name provided.'));
+  if (!item) return Promise.reject(new Error('No item provided.'));
+  if (!storage[schemaName]) storage[schemaName] = {};
+
+  storage[schemaName][item.id] = item;
+
+  return Promise.resolve(item);
+};
+
+exports.getItem = (schemaName, id) => {
+  if (!schemaName) return Promise.reject(new Error('No schema name provided.'));
+  if (!id) return Promise.reject(new Error('No ID provided.'));
+
+  var schema = storage[schemaName];
+  if (!schema) return Promise.reject(new Error('Schema not found.'));
+
+  var item = schema[id];
+  if (!item) return Promise.reject(new Error('No student exists with that ID.'));
+
+  Promise.resolve(item); 
+};
