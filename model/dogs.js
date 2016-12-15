4fdfe88dd1d34f098b3fa484b7902d7bee8e4182
Peter Kim <peterkim23@gmail.com>
2016-12-15 11:14:49 -0800

    all GET and POST test passing

--- model/dogs.js
+++ model/dogs.js
@@ -2,11 +2,13 @@
 
 const uuid = require('node-uuid');
 
-module.exports = function(name, breed) {
+module.exports = function(name, breed, color) {
   if(!name) throw new Error('expected name');
   if(!breed) throw new Error('expected breed');
+  if(!color) throw new Error('expected color');
 
   this.id = uuid.v1();
   this.name = name;
   this.breed = breed;
+  this.color = color;
 };
