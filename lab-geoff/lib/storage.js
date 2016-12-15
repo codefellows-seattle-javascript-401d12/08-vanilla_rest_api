aacc24e868273d7a8f7b2a5968c000059328bee0
Geoff Simons <geoff.simons@gmail.com>
2016-12-15 15:56:08 -0800

    Using bluebird, saving to files

--- lab-geoff/lib/storage.js
+++ lab-geoff/lib/storage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const uuid = require('node-uuid');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
 
 module.exports = exports = {};
 
@@ -12,40 +14,42 @@ exports.createItem = function(collection, item) {
 
   item.id = item.id || uuid.v4().slice(0,8);
 
-  if(!storage[collection]) storage[collection] = {};
+  let json = JSON.stringify(item);
+  return fs.writeFileProm(`./data/${collection}/${item.id}.json`, json)
+  .then( () => item)
+  .catch( err => Promise.reject(err));
 
-  storage[collection][item.id] = item;
-  return Promise.resolve(item);
+  // if(!storage[collection]) storage[collection] = {};
+  //
+  // storage[collection][item.id] = item;
+  // return Promise.resolve(item);
 };
 
 exports.fetchItem = function(collection, id) {
-  return new Promise( (resolve, reject) => {
-    if(!collection) return reject(new Error('collection name not supplied'));
-    if(!id) return reject(new Error('missing id'));
+  if(!collection) return Promise.reject(new Error('collection name not supplied'));
+  if(!id) return Promise.reject(new Error('missing id'));
 
-    var all = storage[collection];
-    if(!all) return reject(new Error(`collection ${collection} does not exist`));
-
-    var item = all[id];
-    if(!item) return reject(new Error(`could not find ${id}`));
-
-    resolve(item);
-  });
+  return fs.readFileProm(`./data/${collection}/${id}.json`)
+  .then( data => {
+    let item = JSON.parse(data.toString());
+    return item;
+  })
+  .catch( err => Promise.reject(err));
 };
 
 exports.deleteItem = function(collection, id) {
-  return new Promise( (resolve, reject) => {
-    if(!collection) return reject(new Error('collection name not supplied'));
-    if(!id) return reject(new Error('missing id'));
+  if(!collection) return Promise.reject(new Error('collection name not supplied'));
+  if(!id) return Promise.reject(new Error('missing id'));
 
-    var all = storage[collection];
-    if(!all) return reject(new Error(`collection ${collection} does not exist`));
+  return fs.unlinkProm(`./data/${collection}/${id}.json`);
 
-    var item = all[id];
-    if(!item) return reject(new Error(`could not find ${id}`));
-
-    delete all[id];
-
-    resolve(item); //Giving back the removed item
-  });
+  // var all = storage[collection];
+  // if(!all) return Promise.reject(new Error(`collection ${collection} does not exist`));
+  //
+  // var item = all[id];
+  // if(!item) return Promise.reject(new Error(`could not find ${id}`));
+  //
+  // delete all[id];
+  //
+  // resolve(item); //Giving back the removed item
 };
