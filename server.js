dcd8aab7df12e3b1bc672e5e53b7bc9d7a1c97e0
Daniel Becker <dbecker4130@gmail.com>
2016-12-14 17:05:11 -0800

    router.js file started, basic constructor complete, now requiring in parse-url and parse-json

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const http = require('http');
+const Router = require('./lib/router.js');
 
 const PORT = process.env.PORT || 3000;
+const router = new Router();
 
 const server = http.createServer();
 
