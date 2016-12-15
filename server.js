46b4bbc6c2baecaa306defdd2269b3c32e32ba9e
Bhavya Bharti <bhavyab@outlook.com>
2016-12-15 13:06:37 -0800

    should returns all of the ids for that resource

---
+++ server.js
@@ -0,0 +1,76 @@
+'use strict';
+
+
+const http = require('http');
+const PORT = 3000;
+
+const Router = require('./lib/router.js');
+const Note = require('./model/note.js');
+const storage = require('./lib/storage.js');
+const router = new Router();
+const server = http.createServer(router.route());
+
+router.get('/api/note', function(req,res){
+  if(req.url.query.id){
+    storage.fetchItem('note', req.url.query.id)
+    .then(note => {
+      res.writeHead(200, {
+        'Content': 'application/json'
+      });
+      res.write(JSON.stringify(note));
+      res.end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+});
+
+router.post('/api/note', function(req,res){
+  try{
+    var note = new Note(req.body.name, req.body.content, req.body.favFood, req.body.place);
+    storage.createItem('note', note);
+    res.write(JSON.stringify(note));
+    res.end();
+  } catch(err){
+    console.error(err);
+    res.writeHead(400, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('bad request');
+    res.end();
+  }
+});
+
+server.listen(PORT, function(){
+  console.log('server is on!!');
+});
+
+router.delete('/api/note', function(res,req){
+  if(req.url.query.id){
+    storage.fetchDel('note', req.body.query.id)
+     .then(note => {
+       res.writeHead(200,{
+         'Content-Type': 'text/plain'
+       });
+       res.write(note);
+       res.write('item deleted!');
+       res.end();
+     } )
+     .catch(err => {
+       console.error(err);
+       res.writeHead(404,{
+         'Content-Type':'text/plain'
+       });
+       res.write('not found');
+       res.end();
+     });
+    return;
+  }
+});
