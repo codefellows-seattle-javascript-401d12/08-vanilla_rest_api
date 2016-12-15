5cc4c08c9997c0ea47138b135f72f84c8dc7eebd
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-15 13:38:26 -0800

    build out route handling in server file

--- server.js
+++ server.js
@@ -3,18 +3,55 @@
 const http = require('http');
 const BEV = require('./model/bevs.js');
 const Router = require('./lib/router.js');
+const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
 router.get('/api/bev', function(req, res) {
-  // TODO: GET data for a vehicle
+  // GET data for a vehicle
+  if (req.url.query.id) {
+    storage.fetchEntry('bev', req.url.query.id)
+    .then( bev => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.write(JSON.stringify(bev));
+      res.end();
+    })
+    .catch( err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('resource not found');
+      res.end();
+    });
+
+    return;
+  }
 });
 
 router.post('/api/bev', function(req, res) {
-  // TODO: POST data for a vehicle
+  // POST data for a vehicle
+  try {
+    var bev = new BEV(req.body.vehicle, req.body.info);
+    storage.createEntry('bev', bev);
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.write(JSON.stringify(bev));
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.writeHead(400, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('bad request');
+    res.end();
+  }
 });
 
-const server = http.createServer();
+const server = http.createServer(router.route());
 
 server.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
