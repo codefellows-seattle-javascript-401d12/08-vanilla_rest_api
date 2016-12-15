c60cc020a7bfcf090f48221790e8132369292e5a
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:31:54 -0800

    Added deleteItem to storage module.

--- lab-steven/lib/storage.js
+++ lab-steven/lib/storage.js
@@ -24,5 +24,19 @@ exports.getItem = (schemaName, id) => {
   var item = schema[id];
   if (!item) return Promise.reject(new Error('No student exists with that ID.'));
 
-  return Promise.resolve(item); 
+  return Promise.resolve(item);
+};
+
+exports.deleteItem = (schemaName, id) => {
+  if (!schemaName) return Promise.reject(new Error('No schema name provided.'));
+  if (!id) return Promise.reject(new Error('No ID provided.'));
+
+  var schema = storage[schemaName];
+  if (!schema) return Promise.reject(new Error('Schema not found.'));
+
+  var item = schema[id];
+  if (!item) return Promise.reject(new Error('No student exists with that ID.'));
+
+  delete storage[schemaName][id];
+  return Promise.resolve(item);
 };
