99c64ea499ea79685438644a9bba0d5729d59ac9
kcirekcom <erick.f.mock@gmail.com>
2016-12-14 21:27:33 -0800

    created route.post in server.js

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
+router.post('/api/pin', function(req, res) {
+  try {
+    var pin = new Pin(req.body.name, req.body.content);
+    storage.createItem('pin', pin);
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.write(JSON.stringify(pin));
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.writeHead(400, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('bad request');
+    res.end();
+  }
+});
+
 const server = http.createServer();
 
 server.listen(PORT, () => {
