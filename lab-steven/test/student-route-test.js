e069e91d97f352b05c0ddc883316719d0727af1e
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:58:20 -0800

    Wrote GET test, started writing DELETE.

--- lab-steven/test/student-route-test.js
+++ lab-steven/test/student-route-test.js
@@ -8,8 +8,8 @@ require('../server.js');
 describe ('Student routes', () => {
   var student = null;
 
-  describe ('POST: /api/student', () => {
-    it('Should return a student', (done) => {
+  describe('POST: /api/student', () => {
+    it('Should return a student', done => {
       request
       .post('localhost:8080/api/student')
       .send({name: 'Steven', age: '30'})
@@ -21,6 +21,26 @@ describe ('Student routes', () => {
         student = response.body;
         done();
       });
+    });
+  });
+
+  describe('GET: /api/student', () => {
+    it ('Should return a student', done => {
+      request
+      .get(`localhost:8080/api/student?id=${student.id}`)
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response.status).to.equal(200);
+        expect(response.body.name).to.equal('Steven');
+        expect(response.body.age).to.equal('30');
+        done();
+      });
+    });
+  });
+
+  describe('DELETE: /api/student', () => {
+    it ('Should return a status of 204 with no content body', done => {
+
     });
   });
 });
