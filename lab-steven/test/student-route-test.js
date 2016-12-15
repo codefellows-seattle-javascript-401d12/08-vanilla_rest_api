9a2aa2c14d0fd496f14c3cec7541e27fb25860d8
Steven Bateman <batemanvo@gmail.com>
2016-12-14 19:55:21 -0800

    Wrote superagent test for POST route.

---
+++ lab-steven/test/student-route-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+
+require('../server.js');
+
+describe ('Student routes', () => {
+  var student = null;
+
+  describe ('POST: /api/student', () => {
+    it('Should return a student', (done) => {
+      request
+      .post('localhost:8080/api/student')
+      .send({name: 'Steven', age: '30'})
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response.status).to.equal(200);
+        expect(response.body.name).to.equal('Steven');
+        expect(response.body.age).to.equal('30');
+        student = response.body;
+        done();
+      });
+    });
+  });
+});
