4984d43261a82e779169a002c5ca7aa4f5dd7fc8
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-15 11:01:37 -0800

    router DELETE

--- lab-jinho/server.js
+++ lab-jinho/server.js
@@ -19,17 +19,13 @@ router.get('/api/note', function(req, res) {
   if (req.url.query.id) {
     storage.fetchItem('note', req.url.query.id)
     .then( note => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
+      res.writeHead(200, {'Content-Type': 'application/json'});
       res.write(JSON.stringify(note));
       res.end();
     })
     .catch( err => {
       console.error(err);
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
+      res.writeHead(404, {'Content-Type': 'text/plain'});
       res.write('not found');
       res.end();
     });
@@ -37,9 +33,7 @@ router.get('/api/note', function(req, res) {
     return;
   };
 
-  res.writeHead(400, {
-    'Content-Type': 'text/plain'
-  });
+  res.writeHead(400, {'Content-Type': 'text/plain'});
   res.write('bad request');
   res.end();
 });
@@ -49,22 +43,36 @@ router.post('/api/note', function(req, res) {
   try {
     var note = new Note(req.body.name, req.body.content);
     storage.createItem('note', note);
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.write(JSON.stringify(note));
     res.end();
   } catch (err) {
     console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain'
-    });
+    res.writeHead(400, {'Content-Type': 'text/plain'});
     res.write('bad request');
     res.end();
   };
 });
 //Logic: Method: DELETE
-
+router.delete('/api/note', function(req, res) {
+  if (req.url.query.id) {
+    storage.deleteItem('note', req.url.query.id)
+    .then(() => {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end();
+    })
+    .catch( err => {
+      console.error(err);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+  res.writeHead(400, {'Content-Type': 'text/plain',});
+  res.write('bad request');
+  res.end();
+});
 
 
 //**START SERVER**
