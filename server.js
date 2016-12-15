c2090bcc51886de28c1c522aa5146568bb8992dd
kcirekcom <erick.f.mock@gmail.com>
2016-12-14 21:20:03 -0800

    added consts in server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const http = require('http');
+const Pin = require('./model/pin.js');
+const Router = require('./lib/router.js');
+const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
+const router = new Router();
 
 const server = http.createServer();
 
