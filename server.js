9daafeb974fc55bf654f6cc9fe41b3777d219222
kcirekcom <erick.f.mock@gmail.com>
2016-12-14 21:47:31 -0800

    created router.get in server.js, api requests are working

--- server.js
+++ server.js
@@ -7,9 +7,31 @@ const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
+router.get('/api/pin', function(req, res) {
+  if (req.url.query.id) {
+    storage.fetchItem('pin', req.url.query.id)
+    .then(pin => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.write(JSON.stringify(pin));
+      res.end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('not found');
+      res.end();
+    });
+    return;
+  }
+});
+
 router.post('/api/pin', function(req, res) {
   try {
-    var pin = new Pin(req.body.name, req.body.content);
+    var pin = new Pin(req.body.title, req.body.skill);
     storage.createItem('pin', pin);
     res.writeHead(200, {
       'Content-Type': 'application/json'
@@ -26,7 +48,7 @@ router.post('/api/pin', function(req, res) {
   }
 });
 
-const server = http.createServer();
+const server = http.createServer(router.route());
 
 server.listen(PORT, () => {
   console.log('server running:', PORT);
