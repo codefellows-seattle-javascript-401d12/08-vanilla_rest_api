4775b083ca8590e9b750df25ea259cf4fe3999d1
Britt Dawn <brittdawndev@gmail.com>
2016-12-15 09:58:37 -0800

    Tests tests tests.

--- test/song-route-test.js
+++ test/song-route-test.js
@@ -9,6 +9,23 @@ describe('Song Routes', function() {
   var song = null;
 
   describe('POST: /api/song', function() {
+    it('should return a 400 bad request if no body is given', function(done) {
+      request.post('localhost:3000/api/song')
+      .send({})
+      .end((err, res) => {
+        expect(err).to.be.an('error');
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+    it('should return a 400 bad request an invalid body is requested', function(done) {
+      request.post('localhost:3000/api/song')
+      .send({ meow: 'meow meow meow', robot: 'beep beep boop' })
+      .end((err, res) => {
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
     it('should return a song', function(done) {
       request.post('localhost:3000/api/song')
       .send({ title: 'test title', description: 'test description' })
@@ -31,6 +48,33 @@ describe('Song Routes', function() {
         expect(res.status).to.equal(200);
         expect(res.body.title).to.equal('test title');
         expect(res.body.description).to.equal('test description');
+        done();
+      });
+    });
+    it('should return a 404 not found for a valid request but an invalid id', function(done) {
+      request.get('localhost:3000/api/song?id=44abc123')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('not found');
+        done();
+      });
+    });
+    it('should return a 400 bad request if id is not provided', function(done) {
+      request.get('localhost:3000/api/song')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
+
+  describe('testing for routes not registered', function() {
+    it('should return a status code of 404', function(done) {
+      request.get('localhost:3000/api/movie')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal('route not found');
         done();
       });
     });
