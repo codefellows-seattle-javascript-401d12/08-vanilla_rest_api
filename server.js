d18b3a6d4a59258c766f892c34ecfa43c6c2786b
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-15 12:48:08 -0800

    set up route handling, write TODOs

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const Router = require('./lib/router.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
 
+router.get('/api/bev', function(req, res) {
+  // TODO: GET data for a vehicle
+});
+
+router.post('/api/bev', function(req, res) {
+  // TODO: POST data for a vehicle
+});
+
 const server = http.createServer();
 
 server.listen(PORT, () => {
