90642896aa17d64e3c15b4f966816311e22b8ccf
Steven Bateman <batemanvo@gmail.com>
2016-12-15 18:40:53 -0800

    Moved routes from server.js to new module.

---
+++ lab-steven/routes/student-route.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Student = require('../model/student-constructor.js');
+const storage = require('../lib/storage.js');
+
+module.exports = function(router) {
+  router.get('/api/student', (request, response) => {
+    if (request.url.query.id) {
+      storage.getItem('student', request.url.query.id)
+      .then(student => {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(student));
+        response.end();
+      })
+      .catch(err => {
+        console.error(err);
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('Student not found.');
+        response.end();
+      });
+      return;
+    }
+    storage.getAllItems()
+    .then(allIds => {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.write(JSON.stringify(allIds));
+      response.end();
+    });
+  });
+
+  router.post('/api/student', (request, response) => {
+    var student = new Student(request.body);
+    storage.createItem('student', student)
+    .then(student => {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      response.write(JSON.stringify(student));
+      response.end();
+    })
+    .catch(err => {
+      console.error(err);
+      response.writeHead(400, {'Content-Type': 'text/plain'});
+      response.write('Bad request.');
+      response.end();
+    });
+  });
+
+  router.delete('/api/student', (request, response) => {
+    if (request.url.query.id) {
+      storage.deleteItem('student', request.url.query.id)
+      .then(() => {
+        response.writeHead(204);
+        response.end();
+      })
+      .catch(err => {
+        console.error(err);
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('Student not found.');
+      });
+      return;
+    }
+  });
+};
