8c38f5cfa4daf8b5d07a5c0aca29f3512e4dc06e
Daniel Becker <dbecker4130@gmail.com>
2016-12-15 16:51:16 -0800

    delete test complete, runs without server. added delete method to storage

--- test/artist-route-test.js
+++ test/artist-route-test.js
@@ -3,17 +3,19 @@
 const request = require('superagent');
 const expect = require('chai').expect;
 
+require('../server.js');
+
 describe('Artist Routes', function() {
   var artist = null;
 
   describe('POST: /api/artist', function() {
     it('should return a artist', function(done) {
       request.post('localhost:4130/api/artist')
-      .send({bandName: 'test name', genre: 'test genre'})
+      .send({name: 'test name', genre: 'test genre'})
       .end((err, response) => {
         if (err) return done(err);
         expect(response.status).to.equal(200);
-        expect(response.body.bandName).to.equal('test name');
+        expect(response.body.name).to.equal('test name');
         expect(response.body.genre).to.equal('test genre');
         artist = response.body;
         done();
@@ -21,10 +23,31 @@ describe('Artist Routes', function() {
     });
   });
 
-//   describe('GET:')
-//   //TODO: create GET test
-//
+  describe('GET: /api/artist', function() {
+    it('should return a artist', function(done) {
+      request.get(`localhost:4130/api/artist?id=${artist.id}`)
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response.status).to.equal(200);
+        expect(response.body.name).to.equal('test name');
+        expect(response.body.genre).to.equal('test genre');
+        done();
+      });
+    });
+  });
 
-//   describe('DELETE:')
-//   //TODO: create DELETE request
+  describe('DELETE: /api/artist', function() {
+    it('should delete an artist', function(done) {
+      // request.delete(`localhost:4130/api/artist?id=${artist.id}`)
+      request.delete(`localhost:4130/api/artist?id=${artist.id}`)
+      .end((err, response) => {
+        // if (err) return done(err);
+        expect(response.status).to.equal(204);
+        expect(response.body.name).to.equal(undefined);
+        expect(response.body.genre).to.equal(undefined);
+        done();
+      });
+    });
+  });
+
 });
