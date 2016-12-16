e4c40a40ff5f242c51abea4aada9f6951b089e11
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-16 11:09:55 -0800

    response refactor complete

--- lab-jinho/route/note-route.js
+++ lab-jinho/route/note-route.js
@@ -5,43 +5,33 @@ const Note = require('../model/note.js');
 const response = require('../lib/response.js');
 
 module.exports = function(router) {
-  
+
   //Logic: Method: GET
   router.get('/api/note', function(req, res) {
     if (req.url.query.id) {
       storage.fetchItem('note', req.url.query.id)
       .then( note => {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify(note));
-        res.end();
+        response.sendJSON(res, 200, note);
       })
       .catch( err => {
         console.error(err);
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('not found');
-        res.end();
+        response.sendText(res, 404, 'not found');
       });
 
       return;
     }
 
-    res.writeHead(400, {'Content-Type': 'text/plain'});
-    res.write('bad request');
-    res.end();
+    response.sendText(res, 400, 'bad request');
   });
   //Logic: Method: POST
   router.post('/api/note', function(req, res) {
     try {
       var note = new Note(req.body.restaurantname, req.body.address);
       storage.createItem('note', note);
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify(note));
-      res.end();
+      response.sendJSON(res, 200, note);
     } catch (err) {
       console.error(err);
-      res.writeHead(400, {'Content-Type': 'text/plain'});
-      res.write('bad request');
-      res.end();
+      response.sendText(res, 400, 'bad request');
     }
   });
   //Logic: Method: DELETE
@@ -60,8 +50,6 @@ module.exports = function(router) {
       });
       return;
     }
-    res.writeHead(400, {'Content-Type': 'text/plain',});
-    res.write('bad request');
-    res.end();
+    response.sendText(res, 400, 'bad request');
   });
 };
