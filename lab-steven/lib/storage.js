c1b33bdfacf8488a3cf92a92706d5eefca238dc7
Steven Bateman <batemanvo@gmail.com>
2016-12-15 19:14:24 -0800

    Refactored deleteItem to getAllItems. Meant to refactor getAllItems first, starting on deleteItem next.

--- lab-steven/lib/storage.js
+++ lab-steven/lib/storage.js
@@ -31,14 +31,9 @@ exports.deleteItem = function(schemaName, id) {
   if (!schemaName) return Promise.reject(new Error('No schema name provided.'));
   if (!id) return Promise.reject(new Error('No ID provided.'));
 
-  var schema = storage[schemaName];
-  if (!schema) return Promise.reject(new Error('Schema not found.'));
-
-  var item = schema[id];
-  if (!item) return Promise.reject(new Error('No student exists with that ID.'));
-
-  delete storage[schemaName][id];
-  return Promise.resolve();
+  fs.readdirProm(`${__dirname}/../data/${schemaName}`)
+  .then(arrayOfFiles => Promise.resolve(arrayOfFiles))
+  .catch(err => Promise.reject(err));
 };
 
 exports.getAllItems = function() {
