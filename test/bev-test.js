ce0056a8bea96f5388bd756b3ed853770d28ae15
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-17 18:22:40 -0800

    write tests for POST, GET (id), GET (all), DELETE functions

---
+++ test/bev-test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+
+require('../server.js');
+
+describe('BEV Routes', function() {
+  var vehicle = null;
+
+  describe('POST: api/bev', function() {
+    it('should return a vehicle info entry', function(done) {
+      request.post('localhost:3000/api/bev')
+      .send({ vehicle: 'Test Vehicle', info: 'Test info' })
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.vehicle).to.equal('Test Vehicle');
+        expect(res.body.info).to.equal('Test info');
+        vehicle = res.body;
+        done();
+      });
+    });
+  });
+
+  describe('GET: api/bev?id=test_id', function() {
+    it('should return a vehicle info entry', function(done) {
+      request.get(`localhost:3000/api/bev?id=${vehicle.id}`)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.vehicle).to.equal('Test Vehicle');
+        expect(res.body.info).to.equal('Test info');
+        done();
+      });
+    });
+  });
+
+  describe('GET: api/bev', function() {
+    it('should return an array of entry ids', function(done) {
+      request.get('localhost:3000/api/bev')
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body[0]).to.equal(vehicle.id);
+        done();
+      });
+    });
+  });
+
+  describe('DELETE: api/bev', function() {
+    it('should delete the test vehicle info entry', function(done) {
+      request.delete(`localhost:3000/api/bev?id=${vehicle.id}`)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).to.equal(204);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.be.empty;
+        done();
+      });
+    });
+  });
+});
