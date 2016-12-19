9e8a1fdf065f15a63e8d5e3b1ab33d53d56fce77
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-19 14:44:06 -0800

    added create and fectch method in restaurant file

--- lab-jinho/model/restaurant.js
+++ lab-jinho/model/restaurant.js
@@ -18,5 +18,16 @@ const Restaurant = module.exports = function(restaurantname, address) {
 
 Restaurant.createRestaurant = function(_restaurant) {
   debug('createRestaurant');
-  
+
+  try {
+    let restaurant = new Restaurant(_restaurant.restaurantname, _restaurant.address);
+    return storage.createItem('restaurant', restaurant);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+Restaurant.fetchRestaurant = function(id) {
+  debug('fetchRestaurant');
+  return storage.fetchItem('restaurant', id);
 };
