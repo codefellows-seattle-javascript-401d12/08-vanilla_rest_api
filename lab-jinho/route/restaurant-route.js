f53e69171d719e14cd3fd8bc78def968639666a1
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-12-30 16:32:36 -0800

    finished writing test and debugging

--- lab-jinho/route/restaurant-route.js
+++ lab-jinho/route/restaurant-route.js
@@ -7,7 +7,7 @@ const List = require('../model/list.js');
 const restaurantRouter = module.exports = new Router();
 
 restaurantRouter.post('/api/list/:listID/restaurant', jsonParser, function(req, res, next) {
-  List.findByIdAndAddNote(req.params.listID, req.body)
+  List.findByIdAndAddRestaurant(req.params.listID, req.body)
   .then( note => res.json(note))
   .catch(next);
 });
